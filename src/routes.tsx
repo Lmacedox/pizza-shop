9e343599a9727d7e51bbab180956ba41b05a03b6
Lucas Macedo <lucasmacedo376@gmail.com>
2024-02-08 21:21:00 -0300

    Create dashboard section This commit is intended for creating the dashboard page/session, the main purpose of the dashboard is to give the customer a quick and compiled view of their sales data and product statistics, helping them to achieve a macro view of what is happening within of your establishment

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/App/dashboard'
+import { Dashboard } from './pages/App/dashboard/dashboard'
 import { Orders } from './pages/App/orders/orders'
 import { SingUp } from './pages/Auth/sign-up'
 import { SingIn } from './pages/Auth/sing-in'
